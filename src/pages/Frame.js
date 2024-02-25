5abc541ac768b8453aec6d69c32a0e7718d7cf3a
abhidada1624 <abhidada1624>
2024-02-25 13:54:37 +0000

    Pushed from Locofy Builder

---
+++ src/pages/Frame.js
@@ -0,0 +1,15 @@
+import "./Frame.css";
+
+const Frame = () => {
+  return (
+    <div className="web-photo-editor-1-parent">
+      <img
+        className="web-photo-editor-1-icon"
+        alt=""
+        src="/web-photo-editor-1@2x.png"
+      />
+    </div>
+  );
+};
+
+export default Frame;
